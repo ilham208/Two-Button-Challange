f2188fdb919265c917bdf8ac2f6c8a8b313df44f
apprecycler <ilhamva@gmail.com>
2022-02-01 19:11:23 -0500

    Initial Commit

---
+++ Two-Button Challange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two-Button Challange
+//
+//  Created by Yilihamu Yisilamu on 2/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
