062c682a24890d409cfba99ba8eae1ebd37a3821
apprecycler <ilhamva@gmail.com>
2022-02-02 15:46:37 -0500

    Completed two button press

--- Two-Button Challange/ViewController.swift
+++ Two-Button Challange/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        messageLabel.text = "You are awesome!"
+        messageLabel.textColor = UIColor.blue
+        messageLabel.textAlignment = .left
+    }
+    
+    @IBAction func showAnotherMessagePressed(_ sender: UIButton) {
+        messageLabel.text = "You are great!"
+        messageLabel.textColor = UIColor.systemRed
+        messageLabel.textAlignment = .right
+    }
+    
 }
 
